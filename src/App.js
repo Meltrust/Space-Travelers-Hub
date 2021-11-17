d09270e25d59a30b856cecca246ae06a2a73e436
Meltrust <original.mtapia@outlook.com>
2021-11-17 00:44:06 -0600

    Add and style header.  Add links and routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">Hello World </div>
+    <div className="container">Hello World </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="container-fluid min-vh-100 bg-light">
-      <App />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="container-fluid min-vh-100 bg-light">
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
